0451abc0cf19c1398e2db8cccd719353ec32d32e
Usuario <Usuario@DESKTOP-ITEJBTN>
2022-02-13 22:56:01 -0600

    tarea

---
+++ src/main/java/com/fgcweb/FgcwebApplication.java
@@ -0,0 +1,13 @@
+package com.fgcweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FgcwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FgcwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fgcweb/FgcwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fgcweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FgcwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
